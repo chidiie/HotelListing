02262cd6de76fedfeed8b5d3c60b73c74dd327c3
Chidinma Ejike <ejikechidi33@gmail.com>
2023-09-28 10:46:52 +0100

    Changed Swagger Bearer UI implementation

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -59,6 +59,31 @@ namespace HotelListing
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
+                
+
+                // Configure JWT authentication for Swagger
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+
             });
 
             services.AddControllers(config => 
